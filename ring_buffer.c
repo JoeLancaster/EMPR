121a22b86c0131d1dd9b54aadabdbab3ad2f2e82
Joe Lancaster <jl1968@york.ac.uk>
2018-02-25 14:28:55 +0000

    fixed ring buffer implementation

--- ring_buffer.c
+++ ring_buffer.c
@@ -1,12 +1,17 @@
 #include "ring_buffer.h"
 #include <stdlib.h>
 
+#define rbh (rb -> head)
+#define rbs (rb -> size)
+#define rbt (rb -> tail)
+
 int rb_is_empty(ring_buf_t * rb) {
-  return (rb -> head) == (rb -> tail);
+  return rbh == rbt;
+ 
 }
 
 int rb_is_full(ring_buf_t * rb) {
-  return ((rb -> head + 1) % rb -> size) == rb -> tail;
+  return ((rbh + 1) % rbs) == rbt;
 }
 
 void rb_de_init(ring_buf_t * rb) {
@@ -15,24 +20,25 @@ void rb_de_init(ring_buf_t * rb) {
 
 void rb_init(ring_buf_t * rb, size_t size) {
   rb -> buffer = malloc(size);
-  rb -> head = 0;
-  rb -> tail = 0;
-  rb -> size = size;
+  rbh = 0;
+  rbt = 0;
+  rbs = size;
 }
 
 void rb_put(ring_buf_t * rb, uint8_t data) {
-  rb -> buffer[rb -> head] = data;
-  rb -> head = (rb -> tail + 1) % rb -> size;
-  if(rb_is_empty(rb)) {
-    rb -> tail = (rb -> tail + 1) % rb -> size;
+  
+  rb -> buffer[rbh] = data;
+  rbh = (rbh + 1) % rbs;
+  if(rbh == rbt) {
+    rbt = (rbt + 1) % rbs;
   }
 }
 
 uint8_t rb_get(ring_buf_t * rb) {
-  uint8_t temp = 22;
+  uint8_t temp = 0;
   if(!rb_is_empty(rb)){
-    temp = rb -> buffer[rb -> tail];
-    rb -> tail = (rb -> tail + 1) % rb -> size;
+    temp = rb -> buffer[rbt];
+    rbt = (rbt + 1) % rbs;
   }
   return temp;
 }
