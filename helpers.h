384dcae618927ae678b21d61b46c671f32245341
johnnysun8 <jpjcoutinho@gmail.com>
2018-03-05 10:33:41 +0000

    removed gen. funcs from helper files

--- helpers.h
+++ helpers.h
@@ -20,10 +20,7 @@ void timer_init(void);
 void turn_on(uint8_t lights);
 void turn_on_single(int light);
 void wait(double seconds);
-void dmx_wait(int time);
 void Systick_Handler(void);
-void TIMER0_IRQHandler(void);
-void TIMER1_IRQHandler(void);
 void Delay(unsigned long tick);
 int read_usb_serial_none_blocking(uint8_t *buf,int length);
 int read_uart1(uint8_t *buf, int length);
@@ -43,21 +40,8 @@ int check_rows(int cols);
 int read_buttons();
 char keypad_char_decode(int button_pattern);
 void myDAC_init();
-int myADC_init(int pinOut);
-void G1();
-void G2_single();
-void G2(int packet[], size_t size);
-void G3(int length);
-int read_intensity(int intensity[], size_t size, int pos);
-int concat(int* arr, size_t len);
-void setup();
-void dmx_clear();
-void show_col(uint8_t col, uint8_t time);
-void show_seq1(int packet1[], int time);
-void show_seq2(int packet1[], int packet2[], int time);
-void show_seq3(int packet1[], int packet2[], int packet3[], int time);
-void show_seq4(int packet1[], int packet2[], int packet3[], int packet4[], int time);
-int key_pressed();
+int myADC_init(int pinOut);  
+
 
 
 #endif
