82d7cee813b8fe18164315eec5dc84245cd34987
johnnysun8 <jpjcoutinho@gmail.com>
2018-03-07 11:33:31 +0000

    back to normal

--- uart1.c
+++ uart1.c
@@ -44,7 +44,7 @@ void uart1_init() {
 }
 
 void dmx_write(int red, int green, int blue) {
- const size_t PACKET_SIZE = 8;
+ const size_t PACKET_SIZE = 4;
  uint8_t packet[PACKET_SIZE];
  memset(packet,0x00,PACKET_SIZE);
  packet[0] = 0x00; //Start code
